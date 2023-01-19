81ebbf6af765441a46d0e3517c0aa0a746243e5d
Akeem <119268984+AkeemS00@users.noreply.github.com>
2023-01-19 18:11:46 -0500

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded',(event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterakeem.azurewebsites.net/api/GetResumeCounter?code=i1MRm4O8r-tvGJCRNtIgXgcvySv7QT5IPZFIld27cQRbAzFuBTFWyw=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response =>{
+    fetch(functionApiUrl).then(response =>{
         return response.json()
     }).then(response =>{
         console.log("Website called function API");
